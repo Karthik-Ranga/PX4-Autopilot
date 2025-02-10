0a894b213008fc7f50d8fc915e743d99e590948f
Karthik-Ranga <you@example.com>
2025-02-10 18:40:41 +0530

    Changed name

--- boards/agamrobotics/can-gps-v1/src/boot_config.h
+++ boards/agamrobotics/can-gps-v1/src/boot_config.h
@@ -65,8 +65,8 @@
 #define OPT_PREFERRED_NODE_ID ANY_NODE_ID
 
 //todo:wrap OPT_x in in ifdefs for command line definitions
-#define OPT_TBOOT_MS                    3000
-#define OPT_NODE_STATUS_RATE_MS         800
+#define OPT_TBOOT_MS                    5000
+#define OPT_NODE_STATUS_RATE_MS         3000
 #define OPT_NODE_INFO_RATE_MS           50
 #define OPT_BL_NUMBER_TIMERS            7
 
